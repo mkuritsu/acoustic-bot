e3c8e2c287090744479fb18ad7772bff855d32d1
mkuritsu <mkuritsuu@gmail.com>
2026-06-09 04:47:49 +0100

    improved command error handling

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,8 @@
 use serenity::{
-    all::{Command, Context, EventHandler, Interaction, Ready},
+    all::{
+        Color, Command, CommandInteraction, Context, CreateEmbed, CreateInteractionResponse,
+        CreateInteractionResponseMessage, EventHandler, Interaction, Ready,
+    },
     async_trait,
 };
 use tracing::{error, info};
@@ -20,11 +23,36 @@ impl EventHandler for CommandHandler {
     }
 
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        let Interaction::Command(command) = &interaction else {
+        let Interaction::Command(cmd) = &interaction else {
             return;
         };
-        if &command.data.name[..] == "play" {
-            play::execute(&ctx, command).await;
+
+        if let Err(e) = self.handle_command(&ctx, cmd).await {
+            let embed = CreateEmbed::new()
+                .title("ERROR")
+                .description(e.to_string())
+                .color(Color::RED);
+
+            let _ = cmd
+                .create_response(
+                    &ctx.http,
+                    CreateInteractionResponse::Message(
+                        CreateInteractionResponseMessage::new().add_embed(embed),
+                    ),
+                )
+                .await;
         }
+    }
+}
+
+impl CommandHandler {
+    async fn handle_command(&self, ctx: &Context, cmd: &CommandInteraction) -> anyhow::Result<()> {
+        match cmd.data.name.as_str() {
+            "play" => play::execute(ctx, cmd).await?,
+            _ => {
+                error!("Unknown command: {}", cmd.data.name);
+            }
+        }
+        Ok(())
     }
 }
--- src/commands/play.rs
+++ src/commands/play.rs
@@ -1,9 +1,10 @@
+use anyhow::Context as _;
 use serenity::all::{
-    Color, CommandInteraction, CommandOptionType, Context, CreateCommand, CreateCommandOption,
-    CreateEmbed, CreateInteractionResponse, CreateInteractionResponseMessage,
-    EditInteractionResponse, ResolvedValue,
+    ChannelId, Color, CommandInteraction, CommandOptionType, Context, CreateCommand,
+    CreateCommandOption, CreateEmbed, CreateInteractionResponse, CreateInteractionResponseMessage,
+    EditInteractionResponse, GuildId, ResolvedValue,
 };
-use songbird::input::{Compose, YoutubeDl};
+use songbird::input::{AuxMetadata, Compose, YoutubeDl};
 use tracing::trace;
 
 use crate::HttpKey;
@@ -17,7 +18,48 @@ pub fn create() -> CreateCommand {
         )
 }
 
-pub async fn execute(ctx: &Context, cmd: &CommandInteraction) {
+pub async fn execute(ctx: &Context, cmd: &CommandInteraction) -> anyhow::Result<()> {
+    let guild_id = cmd
+        .guild_id
+        .context("You need to be on a server to use this command!")?;
+
+    let channel_id = guild_id
+        .get_user_voice_state(&ctx.http, cmd.user.id)
+        .await
+        .ok()
+        .and_then(|voice_state| voice_state.channel_id)
+        .context("You need to be connected to a voice channel to use this command!")?;
+
+    let url = cmd
+        .data
+        .options()
+        .first()
+        .cloned()
+        .context("You need to provide an url!")?;
+    let ResolvedValue::String(url) = url.value else {
+        anyhow::bail!("Invalid url type!");
+    };
+    trace!("Received play for {url} in {channel_id}");
+
+    let _ = cmd
+        .create_response(
+            &ctx.http,
+            CreateInteractionResponse::Defer(CreateInteractionResponseMessage::new()),
+        )
+        .await;
+
+    let metadata = start_playback(ctx, url, guild_id, channel_id).await?;
+    send_reply(ctx, cmd, metadata).await;
+
+    Ok(())
+}
+
+async fn start_playback(
+    ctx: &Context,
+    url: &str,
+    guild_id: GuildId,
+    channel_id: ChannelId,
+) -> anyhow::Result<Option<AuxMetadata>> {
     let manager = songbird::get(ctx)
         .await
         .expect("Should have songbird instance");
@@ -28,118 +70,36 @@ pub async fn execute(ctx: &Context, cmd: &CommandInteraction) {
             .expect("Should have httpclient")
     };
 
-    let Some(guild_id) = cmd.guild_id else {
-        let _ = send_response(
-            cmd,
-            ctx,
-            error_embed("You need to be on a server to use this command!"),
-        )
-        .await;
-        return;
-    };
-
-    let Some(channel_id) = guild_id
-        .get_user_voice_state(&ctx.http, cmd.user.id)
+    let handler = manager
+        .join(guild_id, channel_id)
         .await
-        .ok()
-        .and_then(|voice_state| voice_state.channel_id)
-    else {
-        let _ = send_response(
-            cmd,
-            ctx,
-            error_embed("You need to be connected to a voice channel to use this command!"),
-        )
-        .await;
-        return;
-    };
+        .context("Could not get channel info!")?;
+    let mut handler = handler.lock().await;
 
-    let options = cmd.data.options();
-    let Some(url) = options.first() else {
-        let _ = send_response(cmd, ctx, error_embed("You need to provide an url!")).await;
-        return;
-    };
-    let ResolvedValue::String(url) = url.value else {
-        let _ = send_response(cmd, ctx, error_embed("Invalid url type!")).await;
-        return;
-    };
-    trace!("Received play for {url} in {channel_id}");
-    let _ = send_defer_response(cmd, ctx).await;
-
-    let Ok(handler_lock) = manager.join(guild_id, channel_id).await else {
-        let _ = send_response(cmd, ctx, error_embed("Could not get channel info!")).await;
-        return;
-    };
-
-    let mut handler = handler_lock.lock().await;
     let mut yt_source = YoutubeDl::new(http_client, String::from(url));
-    let meta = yt_source.aux_metadata().await.ok();
+    let _ = handler.play_input(yt_source.clone().into());
 
-    let song_title = meta
+    let meta = yt_source.aux_metadata().await.ok();
+    Ok(meta)
+}
+
+async fn send_reply(ctx: &Context, cmd: &CommandInteraction, metadata: Option<AuxMetadata>) {
+    let song_title = metadata
         .as_ref()
         .and_then(|m| m.title.as_deref())
-        .unwrap_or("<untitled>");
+        .unwrap_or("<unknown title>");
 
-    let song_artist = meta
+    let song_artist = metadata
         .as_ref()
         .and_then(|m| m.artist.as_deref())
         .unwrap_or("<unknown artist>");
 
-    let _ = handler.play_input(yt_source.into());
-    let _ = send_edit_response(
-        cmd,
-        ctx,
-        success_embed(
-            "Now Playing!",
-            format!("I will now play: {song_title} by {song_artist}"),
-        ),
-    )
-    .await;
-}
+    let embed = CreateEmbed::new()
+        .title("Now Playing!")
+        .description(format!("I will now play: {song_title} by {song_artist}"))
+        .color(Color::DARK_GREEN);
 
-fn success_embed(title: impl Into<String>, description: impl Into<String>) -> CreateEmbed {
-    CreateEmbed::new()
-        .title(title)
-        .description(description)
-        .color(Color::DARK_GREEN)
-}
-
-fn error_embed(description: &str) -> CreateEmbed {
-    CreateEmbed::new()
-        .title("ERROR")
-        .description(description)
-        .color(Color::RED)
-}
-
-async fn send_response(
-    cmd: &CommandInteraction,
-    ctx: &Context,
-    embed: CreateEmbed,
-) -> serenity::Result<()> {
-    cmd.create_response(
-        &ctx.http,
-        CreateInteractionResponse::Message(
-            CreateInteractionResponseMessage::new().add_embed(embed),
-        ),
-    )
-    .await?;
-    Ok(())
-}
-
-async fn send_edit_response(
-    cmd: &CommandInteraction,
-    ctx: &Context,
-    embed: CreateEmbed,
-) -> serenity::Result<()> {
-    cmd.edit_response(&ctx.http, EditInteractionResponse::new().add_embed(embed))
-        .await?;
-    Ok(())
-}
-
-async fn send_defer_response(cmd: &CommandInteraction, ctx: &Context) -> serenity::Result<()> {
-    cmd.create_response(
-        &ctx.http,
-        CreateInteractionResponse::Defer(CreateInteractionResponseMessage::new()),
-    )
-    .await?;
-    Ok(())
+    let _ = cmd
+        .edit_response(&ctx.http, EditInteractionResponse::new().add_embed(embed))
+        .await;
 }
